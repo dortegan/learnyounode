aa93311d793eff229f24431f3d251991d6e755fb
Daniel Ortega <dorteganorambuena@gmail.com>
2016-03-24 18:31:24 -0300

    Improve exercise 3 solution

--- exercise3/program.js
+++ exercise3/program.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 var path = process.argv[2];
-var buffer = fs.readFileSync(path);
-var bufferToString = buffer.toString();
-var numberOfLines = bufferToString.split('\n').length - 1
+var fileContent = fs.readFileSync(path, 'utf8');
+var numberOfLines = fileContent.split('\n').length - 1
+
 console.log(numberOfLines);
